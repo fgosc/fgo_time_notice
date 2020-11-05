a296cc8363b582169bf38b3d3e8964531d4b4faf
unknown <fgophi@gmail.com>
2020-11-06 00:05:53 +0900

    JSON形式を変更

--- chalicelib/scraper.py
+++ chalicelib/scraper.py
@@ -7,6 +7,7 @@ import dataclasses
 import json
 import unicodedata
 from typing import List
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -36,11 +37,13 @@ def parse_campaign(url):
         logger.debug("not find title")
         name = ""
 
-    notice = {}
+    notices = []
 
     descs = soup.select('p span.em01')
 #    logger.debug("descs: %s", descs)
     for desc in descs:
+        flag = False
+        notice = {}
         pattern1 = r"^(?P<s_year>20[12][0-9])年(?P<s_month>[0-9]{1,2})月(?P<s_day>[0-9]{1,2})日\([日月火水木金土]\)"
         pattern2 = r" (?P<s_hour>([01][0-9]|2[0-3])):(?P<s_min>[0-5][0-9])"
         pattern3 = r"～(?P<e_month>[0-9]{1,2})月(?P<e_day>[0-9]{1,2})日\([日月火水木金土]\)"
@@ -49,6 +52,7 @@ def parse_campaign(url):
 #        if re.search(pattern1 + pattern2 + pattern3 + pattern4, str(desc)):
         m1 = re.search(pattern, desc.get_text(strip=True))
         if m1:
+            flag = True
             start = re.sub(pattern, r"\g<s_year>/\g<s_month>/\g<s_day> \g<s_hour>:\g<s_min>:00", m1.group())
             end = re.sub(pattern, r"\g<s_year>/\g<e_month>/\g<e_day> \g<e_hour>:\g<e_min>:59", m1.group())
             # 空白にならないところまで親要素をたどる
@@ -64,11 +68,17 @@ def parse_campaign(url):
                 logger.debug(start)
                 logger.debug(end)
                 logger.debug(kikan.get_text(strip=True))
-                notice[name + " " + kikan.get_text(strip=True) + " 開始"] = dt.strptime(start, "%Y/%m/%d %H:%M:%S").timestamp()
-                notice[name + " " + kikan.get_text(strip=True) + " 終了"] = dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp()
+                if time.time() - dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp() < 0:
+                    notice["name"] = name + " " + kikan.get_text(strip=True)
+                    notice["url"] = url
+                    notice["begin"] = int(dt.strptime(start, "%Y/%m/%d %H:%M:%S").timestamp())
+                    notice["end"] = int(dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp())
+                    notices.append(notice)
+        if flag:
+            break
             # logger.info("previous_sibling: %s", desc.previous_sibling.previous_sibling.previous_sibling.previous_sibling)
 
-    return notice
+    return notices
 
 
 def parse_event(url):
@@ -88,11 +98,12 @@ def parse_event(url):
         logger.debug("not find title")
         name = ""
 
-    notice = {}
+    notices = []
 
     descs = soup.select('p span.em01')
 #    logger.debug("descs: %s", descs)
     for desc in descs:
+        notice = {}
         pattern1 = r"^(?P<s_year>20[12][0-9])年(?P<s_month>[0-9]{1,2})月(?P<s_day>[0-9]{1,2})日\([日月火水木金土]\)"
         pattern2 = r" (?P<s_hour>([01][0-9]|2[0-3])):(?P<s_min>[0-5][0-9])"
         pattern3 = r"～(?P<e_month>[0-9]{1,2})月(?P<e_day>[0-9]{1,2})日\([日月火水木金土]\)"
@@ -116,11 +127,15 @@ def parse_event(url):
                 logger.debug(start)
                 logger.debug(end)
                 logger.debug(kikan.get_text(strip=True))
-                notice[name + " " + kikan.get_text(strip=True) + " 開始"] = dt.strptime(start, "%Y/%m/%d %H:%M:%S").timestamp()
-                notice[name + " " + kikan.get_text(strip=True) + " 終了"] = dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp()
+                if time.time() - dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp() < 0:
+                    notice["name"] = name + " " + kikan.get_text(strip=True)
+                    notice["url"] = url
+                    notice["begin"] = int(dt.strptime(start, "%Y/%m/%d %H:%M:%S").timestamp())
+                    notice["end"] = int(dt.strptime(end, "%Y/%m/%d %H:%M:%S").timestamp())
+                    notices.append(notice)
             # logger.info("previous_sibling: %s", desc.previous_sibling.previous_sibling.previous_sibling.previous_sibling)
 
-    return notice
+    return notices
 
 
 def parse_page(load_url):
@@ -137,10 +152,10 @@ def parse_page(load_url):
         if word in page_title:
             return None
     if "キャンペーン" in page_title:
-        notice = parse_campaign(load_url)
+        notices = parse_campaign(load_url)
     else:
-        notice = parse_event(load_url)
-    return notice
+        notices = parse_event(load_url)
+    return notices
 
 
 def get_pages(url):
@@ -148,22 +163,21 @@ def get_pages(url):
     html = requests.get(url)
     soup = BeautifulSoup(html.content, "html.parser")
     tag_item = soup.select('ul.list_news li a')
-    notice = {}
+    notices = []
 
     for tag in tag_item:
         load_url = base_url + tag.get("href")
         logger.debug(load_url)
-        dic = parse_page(load_url)
-        if dic is not None:
-            notice.update(dic)
-    notice = sorted(notice.items(), key=lambda x:x[1])
-    return notice
+        event_list = parse_page(load_url)
+        if event_list is not None:
+            notices += event_list
+    return notices
 
 def main():
     # Webページを取得して解析する
     news_url = "https://news.fate-go.jp"
-    notice = get_pages(news_url)
-    data = json.dumps(notice, ensure_ascii=False)
+    notices = get_pages(news_url)
+    data = json.dumps(notices, ensure_ascii=False)
     print(data)
 
 
